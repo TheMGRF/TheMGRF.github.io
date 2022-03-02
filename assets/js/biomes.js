9b975882dc5c7efec16a8b4300bd845b86f208c5
TheMGRF <thomas.grf@googlemail.com>
2022-03-02 17:48:52 +0000

    Particle QOL, options for percipitation, and json fixes

--- assets/js/biomes.js
+++ assets/js/biomes.js
@@ -22,17 +22,17 @@ function submit(formData) {
         "\"category\": \"none\",",
         "\"effects\": {",
         "  \"particle\": {",
-        "   \"options\": {",
-        "    \"type\": \"minecraft:{}\"",
-        "   },",
-        "   {},",
+        "    \"options\": {",
+        "      \"type\": \"minecraft:{}\"",
+        "    },",
+        "    {}",
         "  },",
         "  {},",
         "  {},",
         "  {},",
         "  {},",
         "  {},",
-        "  {},",
+        "  {}",
         "},",
         "\"starts\": [],",
         "\"spawners\": {},",
@@ -60,13 +60,12 @@ function submit(formData) {
         const slot = slots[i - 1];
         console.log("SLOT: " + slot)
         const labelText = entry.name;
-        if (type === "number" || type === "color") {
+        if (type === "number" || type === "color" || (type === "select-one" && labelText !== "particle")) {
             const val = "\"" + labelText + "\": " + value;
             data[slot] = data[slot].replaceAll("{}", val);
             console.log(val);
         } else {
             if (labelText === "particle") {
-                //const output = "\"type\": \"minecraft:" + value + "\"";
                 const val = data[slot].replaceAll("{}", value);
                 data[slot] = val;
                 console.log(val);
@@ -111,3 +110,97 @@ function downloadFile(filename, text) {
 
     document.body.removeChild(element);
 }
+
+const particles = [
+    "ambient_entity_effect",
+    "angry_villager",
+    "ash",
+    "barrier",
+    "block",
+    "bubble",
+    "bubble_pop",
+    "bubble_column_up",
+    "campfire_cosy_smoke",
+    "campfire_signal_smoke",
+    "cloud",
+    "composter",
+    "crimson_spore",
+    "crit",
+    "current_down",
+    "damage_indicator",
+    "dolphin",
+    "dragon_breath",
+    "dripping_dripstone_lava",
+    "dripping_dripstone_water",
+    "dripping_lava",
+    "dripping_obsidian_tear",
+    "dripping_water",
+    "dust",
+    "dust_color_transition",
+    "effect",
+    "elder_guardian",
+    "electric_spark",
+    "enchant",
+    "enchanted_hit",
+    "end_rod",
+    "entity_effect",
+    "explosion_emitter",
+    "explosion",
+    "falling_dripstone_lava",
+    "falling_dripstone_water",
+    "falling_dust",
+    "falling_lava",
+    "falling_obsidian_tear",
+    "falling_spore_blossom",
+    "falling_water",
+    "firework",
+    "fishing",
+    "flame",
+    "flash",
+    "glow",
+    "glow_squid_ink",
+    "happy_villager",
+    "heart",
+    "instant_effect",
+    "item",
+    "item_slime",
+    "item_snowball",
+    "landing_lava",
+    "landing_obsidian_tear",
+    "large_smoke",
+    "lava",
+    "light",
+    "mycelium",
+    "nautilus",
+    "note",
+    "poof",
+    "portal",
+    "rain",
+    "scrape",
+    "smoke",
+    "sneeze",
+    "snowflake",
+    "soul",
+    "soul_fire_flame",
+    "spit",
+    "splash",
+    "spore_blossom_air",
+    "squid_ink",
+    "sweep_attack",
+    "totem_of_undying",
+    "underwater",
+    "vibration",
+    "warped_spore",
+    "wax_off",
+    "wax_on",
+    "white_ash",
+    "witch"
+]
+
+particles.forEach(particle => {
+    let opt = document.createElement("option");
+    opt.value = particle;
+    opt.textContent = particle.toUpperCase();
+
+    document.getElementById("particle").append(opt)
+});
